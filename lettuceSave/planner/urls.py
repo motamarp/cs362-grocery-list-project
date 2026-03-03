8820aa08f6fe95a1f62defedc9e43e4fabe86d29
HilarionChristian Gunawan <gunawanh@flip4.engr.oregonstate.edu>
2026-03-02 23:34:25 -0800

    added planner views, urls, serializers for mealplan, grocerylist, recipe

--- lettuceSave/planner/urls.py
+++ lettuceSave/planner/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from . import views
 
 router = DefaultRouter()
+router.register(r'recipes', views.RecipeViewSet)
+router.register(r'mealplans', views.MealPlanViewSet)
+router.register(r'groceries', views.GroceryListViewSet)
+router.register(r'plannedmeals', views.PlannedMealViewSet)
+router.register(r'stores', views.StoreViewSet)    
+router.register(r'store-ingredients', views.StoreIngredientViewSet) 
+router.register(r'grocery-list-items', views.GroceryListItemViewSet) 
 
 urlpatterns = [
     path('', include(router.urls)),
--- lettuceSave/planner/views.py
+++ lettuceSave/planner/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
+from .models import Recipe, MealPlan, GroceryList,  PlannedMeal, Store, StoreIngredient, GroceryListItem      
+from .serializers import RecipeSerializer, MealPlanSerializer, GroceryListSerializer, PlannedMealSerializer, StoreSerializer, StoreIngredientSerializer, GroceryListItemSerializer  
 
-# Create your views here.
+class RecipeViewSet(viewsets.ModelViewSet):
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
+    permission_classes = [AllowAny]   # For testing only; remove in production
+
+class MealPlanViewSet(viewsets.ModelViewSet):
+    queryset = MealPlan.objects.all()
+    serializer_class = MealPlanSerializer
+    permission_classes = [AllowAny]
+
+class GroceryListViewSet(viewsets.ModelViewSet):
+    queryset = GroceryList.objects.all()
+    serializer_class = GroceryListSerializer
+    permission_classes = [AllowAny]
+
+class PlannedMealViewSet(viewsets.ModelViewSet):
+    queryset = PlannedMeal.objects.all()
+    serializer_class = PlannedMealSerializer
+    permission_classes = [AllowAny]
+    
+####################################################added
+class StoreViewSet(viewsets.ModelViewSet):
+    queryset = Store.objects.all()
+    serializer_class = StoreSerializer
+    permission_classes = [AllowAny]
+
+class StoreIngredientViewSet(viewsets.ModelViewSet):
+    queryset = StoreIngredient.objects.all()
+    serializer_class = StoreIngredientSerializer
+    permission_classes = [AllowAny]
+
+
+class GroceryListItemViewSet(viewsets.ModelViewSet):  
+    queryset = GroceryListItem.objects.all()
+    serializer_class = GroceryListItemSerializer
+    permission_classes = [AllowAny]
