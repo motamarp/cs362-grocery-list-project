6b2a8b45efaf901432420801ee84ef8112dc1b05
Ahman Raines <rainesah@flip1.engr.oregonstate.edu>
2026-03-06 14:19:05 -0800

    Added missing import statements lost in merge

--- lettuceSave/lettuce_app/serializers.py
+++ lettuceSave/lettuce_app/serializers.py
@@ -1,4 +1,6 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
+from .models import UserProfile
 
 # Create your serializers here.
 class UserProfileSerializer(serializers.ModelSerializer):
